f7a8b2fd536f81efec444414f06ecd6ce3f068e0
Vinh Thai <thaiB2014786@student.ctu.edu.vn>
2024-04-23 20:01:54 +0700

    .

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <!-- <form class="groupSearch" role="search">
+                  <input class="inputSearch" type="search" placeholder="Search" aria-label="Search">
+                  <span class="iconSearch"><i class="fa-solid fa-magnifying-glass"></i></span>
+              </form> -->
+        <div class="infoUser">
+          <!-- <div class="btnLogin">Login</div> -->
+          <div v-if="isUserLoggedIn()" class="contentInfo">
+            <img :src="imageUser" class="imageUser" alt="noImage" />
+            <span class="nameUser">
+              {{ username }}
+            </span>
+            <!-- <button type="button" class="btnCart">
+                          <i class="fa-solid fa-cart-shopping m-auto"></i>
+                          <span style="opacity: 0.7;"
+                              class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
+                              10
+                          </span>
+                      </button> -->
+          </div>
+          <div v-else>
+            <router-link to="/login" class="text-decoration-none">
+              <button class="btnLogin">Đăng nhập</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup>
+  const isUserLoggedIn = () => {
+    return localStorage.getItem("isLoginDG") === "true";
+  };
+  const username = localStorage.getItem("TenDG");
+  const Avatar = localStorage.getItem("AvatarDG");
+  const imageUser = `http://localhost:3000/${Avatar}`;
+  </script>
+  
+  <style lang="scss" scoped>
+  @import "AppHeader.scss";
+  </style>
---
+++ src/components/AuthHeader/AuthHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="title">
+      <div class="logo">
+        <img src="../../assets/logoBook.png" alt="Vue" />
+        <span class="titleWeb">BOOKS<span class="text-dark">FUTURE</span></span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "AuthHeader.scss";
+</style>
